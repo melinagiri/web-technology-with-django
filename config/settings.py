e3e9cc275c31d3a7be24b55e04afe290ceb29016
Melina Giri <melig9539@gmail.com>
2026-09-10 19:35:49 +0545

    migrations changesto db schema

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +75,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('geetshala'),
+#         'USER': os.getenv('melig'),
+#         'PASSWORD': os.getenv('melina@123'),
+#         'HOST': os.getenv('DB_HOST', 'localhost'),  # Default to localhost if not set
+#         'PORT': os.getenv('DB_PORT', '3306'),      # Default to 3306 if not set
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'geetshala',
+        'USER':'djangouser',
+        'PASSWORD': 'djangopassword',
+        'HOST': 'localhost',  # Default to localhost if not set
+        'PORT': '3306',      # Default to 3306 if not set
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)   # maps to a database column
+    lyrics = models.TextField()
+    release_date = models.DateField(null=True, blank=True)
+    is_published = models.BooleanField(default=False)
